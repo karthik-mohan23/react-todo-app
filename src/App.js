77a99a878a746a0debe691d7c8c6982136476637
karthik-mohan23 <karthikmohan2310@gmail.com>
2023-07-10 16:15:32 +0530

    created AddTask component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import ShowTasks from "./components/ShowTasks";
+import AddTask from "./components/AddTask";
 
 function App() {
   const [tasks, setTasks] = useState(["Task 1", "Task 2"]);
 
   return (
-    <div>
+    <div className="mt-28">
+      <AddTask />
       {tasks.map((task) => (
         <ShowTasks task={task} />
       ))}
--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,9 +1,15 @@
 const AddTask = () => {
   return (
-    <div>
-      <form>
-        <input type="text" />
-        <button></button>
+    <div className="w-[90%] max-w-lg mx-auto mb-10">
+      <form className="flex items-center gap-4 w-full">
+        <input
+          type="text"
+          placeholder="Add task"
+          className="border border-gray-400 rounded-md w-full px-3 h-10 focus:outline-blue-400"
+        />
+        <button className="bg-green-500 text-white py-2 px-4 rounded-md">
+          ADD
+        </button>
       </form>
     </div>
   );
