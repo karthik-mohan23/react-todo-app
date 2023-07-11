8f8ee69ff921be95e65996d7f6ed37321745bd9a
karthik-mohan23 <karthikmohan2310@gmail.com>
2023-07-11 19:56:00 +0530

    add delete item  functionality

--- src/App.js
+++ src/App.js
@@ -5,11 +5,15 @@ import AddTask from "./components/AddTask";
 function App() {
   const [tasks, setTasks] = useState([]);
 
+  const deleteTask = (id) => {
+    setTasks((prevTasks) => prevTasks.filter((task, index) => index !== id));
+  };
+
   return (
     <div className="mt-28">
       <AddTask setTasks={setTasks} tasks={tasks} />
       {tasks.map((task, index) => (
-        <ShowTasks task={task} key={index} />
+        <ShowTasks key={index} task={task} deleteTask={deleteTask} id={index} />
       ))}
     </div>
   );
--- src/components/ShowTasks.js
+++ src/components/ShowTasks.js
@@ -1,8 +1,19 @@
-const ShowTasks = ({ task }) => {
+import { AiFillDelete } from "react-icons/ai";
+
+const ShowTasks = ({ task, id, deleteTask }) => {
   return (
     <div className="w-[90%] max-w-lg mx-auto flex items-center gap-4">
-      <input type="checkbox" id="checkbox" />
-      <h1>{task}</h1>
+      <div className="flex items-center justify-between w-full">
+        <div className="flex items-center gap-4">
+          <input type="checkbox" id="checkbox" />
+          <h1>{task}</h1>
+        </div>
+
+        <AiFillDelete
+          onClick={() => deleteTask(id)}
+          className="text-red-600 cursor-pointer"
+        />
+      </div>
     </div>
   );
 };
